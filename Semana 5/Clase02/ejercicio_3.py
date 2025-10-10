ec5bd7b51da41d8908142f456b170dca9823bf8b
vicrenlopezh14 <victor.lopez@echotechs.net>
2025-10-09 19:12:27 -0600

    Ejercicio 3.

--- Semana 5/Clase02/ejercicio_3.py
+++ Semana 5/Clase02/ejercicio_3.py
@@ -2,10 +2,17 @@
 6 peticiones para su fruta favorita.
 """
 
+kids=list()
+
 # Six times
 for i in range(6):
-    # Ask them
-    fruta=input(f"¿Cuál es la fruta favorita del niño {i + 1}?: ")
+    # Ask what kid it is
+    kidNum= int(input("¿De qué niño estamos hablando (número)?: "))
 
-    # Print it
-    print(f"La fruta favorita del niño {i + 1} es {fruta}.")
+    kids.append(input(f"¿Cuál es la fruta favorita del niño {i + 1}?: "))
+
+# Print them
+for i in range(len(kids)):
+    print(f"La fruta favorita del niño número {i} es {kids[i]}")
+
+# Esto está malo, no le entendí a alvin jajaja
