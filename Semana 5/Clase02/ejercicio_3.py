f27aae0d58561e39fd1e4e38f04f942ae0585ed0
vicrenlopezh14 <victor.lopez@echotechs.net>
2025-10-09 19:05:42 -0600

    Ejercicio 3.

---
+++ Semana 5/Clase02/ejercicio_3.py
@@ -0,0 +1,11 @@
+"""
+6 peticiones para su fruta favorita.
+"""
+
+# Six times
+for i in range(6):
+    # Ask them
+    fruta=input(f"¿Cuál es la fruta favorita del niño {i + 1}?: ")
+
+    # Print it
+    print(f"La fruta favorita del niño {i + 1} es {fruta}.")
